b8fd1b5559d0ed3919efb74817f75ca081c381ec
SimDev <simon.b.kurk@gmail.com>
2026-05-21 14:48:23 +0200

    feat: ein bisschen mit dem Text, -farbe, -größe, -position experimentiert

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,12 @@ void setup()
   Serial.begin(115200);
   delay(1000);
   Serial.println("Fahrrad-Computer startet...");
-
-  // Manuelle Initialisierung der Pins (optional, aber sicher ist sicher)
   pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH); // Reset Pin auf HIGH
+  digitalWrite(4, HIGH);
 
   Serial.println("Initialisiere TFT...");
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(3);
 
   Serial.println("Bildschirm blau machen");
   tft.fillScreen(TFT_BLUE);
@@ -23,4 +21,19 @@ void setup()
 
 void loop()
 {
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.drawString("Hello World", 50, 10); // aufbau: text dann x und y
+
+  tft.setTextColor(TFT_BROWN);
+  tft.setTextSize(3);
+  tft.drawString("Helllooo World", 50, 25);
+
+  tft.setTextColor(TFT_DARKGREY, TFT_CYAN);
+  tft.setTextSize(1);
+  tft.drawString("Hey World", 50, 50);
+
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.drawString("Test #" + String(2), 70, 60); // man kann leider nicht einfach Ziffern normal anzeigen
 }
