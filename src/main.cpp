2dab8ac0dff425a9850d07724af5d4706d8cce4e
SimDev <simon.b.kurk@gmail.com>
2026-05-21 15:19:11 +0200

    feat: weiter experimentiert, aber nun wird ein strichmännchen angezeigt

--- src/main.cpp
+++ src/main.cpp
@@ -15,32 +15,25 @@ void setup()
   tft.setRotation(3);
   tft.fillScreen(TFT_BLACK);
   Serial.println("Initialisierung abgeschlossen!");
+
+  tft.setTextColor(TFT_BLUE);
+  tft.setTextSize(2);
+  tft.setCursor(60, 10);
+  tft.print("My Stickman");
+
+  tft.drawLine(0, 35, 320, 35, TFT_WHITE);
+
+  tft.drawCircle(160, 75, 20, TFT_YELLOW);
+
+  tft.drawLine(160, 95, 160, 155, TFT_WHITE);
+
+  tft.drawLine(120, 115, 200, 115, TFT_WHITE);
+
+  tft.drawLine(160, 155, 130, 210, TFT_WHITE);
+
+  tft.drawLine(160, 155, 190, 210, TFT_WHITE);
 }
 
 void loop()
 {
-  tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(2);
-  tft.drawString("Hello World", 50, 10); // aufbau: text dann x und y
-
-  tft.setTextColor(TFT_BROWN);
-  tft.setTextSize(3);
-  tft.drawString("Helllooo World", 50, 25);
-
-  tft.setTextColor(TFT_DARKGREY, TFT_CYAN);
-  tft.setTextSize(1);
-  tft.drawString("Hey World", 50, 50);
-
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(2);
-  tft.drawString("Test #" + String(2), 70, 60); // man kann leider nicht einfach Ziffern normal anzeigen
-
-  tft.drawRect(60, 200, 80, 300, TFT_DARKCYAN); // so ist der aufbau tft.drawRect(x, y, breite, hoehe, farbe);
-  tft.fillRect(10, 10, 10, 10, TFT_CASET);
-  tft.drawRoundRect(20, 20, 20, 20, 40, TFT_YELLOW);
-
-  tft.fillCircle(160, 120, 30, TFT_BLUE); // tft.fillCircle(x_mitte, y_mitte, radius, farbe); oder drawCircle
-  tft.drawCircle(200, 40, 70, TFT_DARKGREY);
-
-  tft.drawLine(0, 50, 320, 50, TFT_WHITE); // tft.drawLine(x_start, y_start, x_ende, y_ende, farbe);gfggfgffgfgfghfghhfghhghg
 }
