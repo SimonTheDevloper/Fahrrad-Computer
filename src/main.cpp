c1a7ef6932758b216ab7851cdcc3e557ee6c3a60
SimDev <simon.b.kurk@gmail.com>
2026-05-21 15:01:01 +0200

    feat: paar kreise, linien, Rechtecke hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,7 @@ void setup()
   Serial.println("Initialisiere TFT...");
   tft.init();
   tft.setRotation(3);
-
-  Serial.println("Bildschirm blau machen");
-  tft.fillScreen(TFT_BLUE);
+  tft.fillScreen(TFT_BLACK);
   Serial.println("Initialisierung abgeschlossen!");
 }
 
@@ -36,4 +34,13 @@ void loop()
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(2);
   tft.drawString("Test #" + String(2), 70, 60); // man kann leider nicht einfach Ziffern normal anzeigen
+
+  tft.drawRect(60, 200, 80, 300, TFT_DARKCYAN); // so ist der aufbau tft.drawRect(x, y, breite, hoehe, farbe);
+  tft.fillRect(10, 10, 10, 10, TFT_CASET);
+  tft.drawRoundRect(20, 20, 20, 20, 40, TFT_YELLOW);
+
+  tft.fillCircle(160, 120, 30, TFT_BLUE); // tft.fillCircle(x_mitte, y_mitte, radius, farbe); oder drawCircle
+  tft.drawCircle(200, 40, 70, TFT_DARKGREY);
+
+  tft.drawLine(0, 50, 320, 50, TFT_WHITE); // tft.drawLine(x_start, y_start, x_ende, y_ende, farbe);gfggfgffgfgfghfghhfghhghg
 }
