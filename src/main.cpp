881af29244c184923611c25374362df398714eda
SimDev <simon.b.kurk@gmail.com>
2026-07-02 16:11:49 +0200

    learn: wie der Esp32 im Acces point einen Webserver starten kann

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
+
+WebServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
     delay(1000);
+    Serial.print("Acces mode wird gestartet");
 
-    Serial.println("Wlan wird gestartet");
+    WiFi.mode(WIFI_AP); // damiit wird dem Chip gesagt das er im Acces Point mode sein soll
 
-    WiFi.softAP("BikeComputer", "SimDev123"); // damit er den Acces Point startet mit Name, Passwort
-    Serial.println("Wlan erfolgreich gestartet.");
+    WiFi.softAP("BikeComputer", "SimDev123");
+    delay(100);
 
-    Serial.print("IP adresse vom ESP32: ");
-    Serial.println(WiFi.softAPIP()); // damit gibt er die IP
+    Serial.println("WLAN erfolgreich gestartet.");
+    Serial.print("IP-Adresse vom ESP32: ");
+
+    Serial.println(WiFi.softAPIP()); // damit gibt er seine IP raus
+
+    server.on("/", []()
+              { server.send(200, "text/plain", "HELLO client! Ich bin im AP mdus!"); });
+
+    server.begin(); // damit wird dann der Webserver gestartet
+    Serial.println("Webserver im AP-Modus gestartet!");
 }
 
 void loop()
 {
+    server.handleClient(); // damit schaut der ob die ganze Zeit Clients sich verbinden wollen.
 }
