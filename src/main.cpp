965c5d07f4fe4e413cd1afa222af166a5b3001c6
SimDev <simon.b.kurk@gmail.com>
2026-07-10 16:44:23 +0200

    learn: wie man Temperatur und Höhe von einem BMP280 sensor bekommt

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,32 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <Preferences.h>
-#include "secrets.h"
-#include <FS.h>
-#include <LittleFS.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 
-Preferences preferences;
-String ssid;
-String password;
-#define LED_PIN 15
-
-WebServer server(80);
-
-void ladeWifiConfig()
-{
-    preferences.begin("config", true); // damit öffnet es einen Namespace und nur im Lesemodus
-
-    ssid = preferences.getString("ssid", "");
-    password = preferences.getString("password", "");
-
-    preferences.end(); // damit es auch wieder schließt um ressourchen zu sparen
-}
-void speicherWifiConfig(String newSsid, String newPassword)
-{
-    preferences.begin("config", false);
-
-    if (!preferences.isKey("ssid")) // damit es nur einmal gespeichert wird
-    {
-        preferences.putString("ssid", WIFI_SSID);
-    }
-
-    if (!preferences.isKey("password"))
-    {
-        preferences.putString("password", WIFI_PASSWORD);
-    }
-}
-void starteWifi()
-{
-    speicherWifiConfig(ssid, password);
-
-    ladeWifiConfig();
-    WiFi.mode(WIFI_AP_STA);                     // damiit wird dem Chip gesagt das er in beiden Modes mode sein soll
-    WiFi.begin(ssid.c_str(), password.c_str()); // damit startet er dann fürs Heimnetzt
-
-    WiFi.softAP("BikeComputer", "SimDev123");
-}
-void handleLedOn()
-{
-    digitalWrite(LED_PIN, HIGH);
-    server.send(200, "application/json", "{\"status\":\"on\"}");
-}
-void handleLedOff()
-{
-    digitalWrite(LED_PIN, LOW);
-    server.send(200, "application/json", "{\"status\":\"off\"}");
-}
-void handleRoute()
-{
-    File datei = LittleFS.open("/index.html", "r");
-
-    if (!datei)
-    {
-        server.send(404, "text/plain", "Datei nicht gefunden");
-        return;
-    }
-
-    String inhalt = datei.readString();
-    datei.close();
-
-    server.send(200, "text/html", inhalt);
-}
+Adafruit_BMP280 bmp;
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(LED_PIN, OUTPUT);
-    if (!LittleFS.begin(true))
+
+    if (!bmp.begin(0x76))
     {
-        Serial.println("LittleFS could not be started");
-        return;
+        Serial.println("BMP280 not found");
+        while (1)
+            ;
     }
-    delay(1000);
-
-    Serial.print("Acces mode wird gestartet");
-    starteWifi();
-    delay(100);
-
-    Serial.println("WLAN erfolgreich gestartet.");
-    Serial.print("IP-Adresse vom ESP32: ");
-
-    Serial.println(WiFi.softAPIP()); // damit gibt er seine IP raus
-
-    server.on("/", handleRoute);
-    server.serveStatic("/script.js", LittleFS, "/script.js");
-    server.on("/tests/ledon", handleLedOn);
-    server.on("/tests/ledoff", handleLedOff);
-
-    server.begin(); // damit wird dann der Webserver gestartet
-    Serial.println("Webserver im AP und STA gestartet!");
 }
-
 void loop()
 {
-    server.handleClient(); // damit schaut der ob die ganze Zeit Clients sich verbinden wollen.
+    Serial.print("Temperatur: ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Druck: ");
+    Serial.print(bmp.readPressure() / 100.0); // damit man hPascal bekommt
+    Serial.println(" hPa");
+
+    float hoehe = bmp.readAltitude(1013.25); // führ meereshöhe
+    Serial.print(hoehe);
+    Serial.println(" m");
+
+    delay(1000);
 }
