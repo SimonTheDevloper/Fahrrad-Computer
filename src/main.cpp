01ae2418f93e61258bd9f4c6b57c083b464bbd4f
SimDev <simon.b.kurk@gmail.com>
2026-07-16 14:01:37 +0200

    learn: gelert wie man mit einer SD-Karte schreiben kann

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,35 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 #include <Arduino.h>
-#include <FS.h>
-#include <LittleFS.h>
-#include <TFT_eSPI.h>
-#include <TJpg_Decoder.h>
 
-TFT_eSPI tft = TFT_eSPI();
-
-bool tft_output(int16_t x, int16_t y, uint16_t width, uint16_t height, uint16_t *bitmap)
-{
-
-    tft.pushImage(x, y, width, height, bitmap); // Zeichnet den aktuellen bitmap
-
-    return true;
-}
+#define SD_CS_PIN 15
 
 void setup()
 {
     Serial.begin(115200);
-    tft.init();
+    SPI.begin(18, 19, 23, SD_CS_PIN);
 
-    if (!LittleFS.begin())
+    if (!SD.begin(SD_CS_PIN))
     {
-        Serial.println("LittleFS konnte nicht gestartet werden");
+        Serial.println("SD-Karte konnte nicht initialisiert werden!");
         return;
     }
+    Serial.println("SD Karte ist bereit");
 
-    tft.setRotation(3);
+    File meineCooleDatei = SD.open("/test.txt", FILE_WRITE);
 
-    TJpgDec.setCallback(tft_output); // damit weiß der Decoder wo hin mit dem bil
-
-    TJpgDec.setJpgScale(1);
-    TJpgDec.setSwapBytes(true);
-
-    TJpgDec.drawFsJpg(0, 0, "/geralt-binary-code.jpg", LittleFS); // Zeichnet das Bild aus dem LittleFs und mit den Koordinaten
+    if (meineCooleDatei)
+    {
+        meineCooleDatei.println("Hier steht was cooles!");
+        meineCooleDatei.close();
+        Serial.println("Fertig geschireben und geschlossen");
+    }
+    else
+    {
+        Serial.println("Fehler beim schreiben!");
+    }
 }
-
 void loop()
 {
 }
