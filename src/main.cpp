a99cfe83ece2e2f84cead6893baad1135b4b7257
SimDev <simon.b.kurk@gmail.com>
2026-07-21 21:32:37 +0200

    learn: wie man aus der SD karte ein Bild öffnet

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,47 @@
 #include "SPI.h"
 #include <Arduino.h>
 
-#define SD_CS_PIN 15
+#define SD_CS_PIN 13
 
 void setup()
 {
     Serial.begin(115200);
+
+    delay(500);
+
     SPI.begin(18, 19, 23, SD_CS_PIN);
 
-    if (!SD.begin(SD_CS_PIN))
+    if (!SD.begin(SD_CS_PIN, SPI, 4000000))
     {
         Serial.println("SD-Karte konnte nicht initialisiert werden!");
         return;
     }
-    Serial.println("SD Karte ist bereit");
+    Serial.println("SD-Karte ist bereit.");
 
-    File meineCooleDatei = SD.open("/test.txt", FILE_WRITE);
+    File mapDatei = SD.open("/testMap.jpg", FILE_READ);
 
-    if (meineCooleDatei)
+    if (mapDatei)
     {
-        meineCooleDatei.println("Hier steht was cooles!");
-        meineCooleDatei.close();
-        Serial.println("Fertig geschireben und geschlossen");
+        Serial.println("Datei 'testMap.jpg' erfolgreich geöffnet!");
+        Serial.print("Dateigröße: ");
+        Serial.print(mapDatei.size());
+        Serial.println(" Bytes");
+
+                /*
+        while (mapDatei.available()) {
+        hier könnte ich es dann mit den TFT display coden.
+        }
+        */
+
+        mapDatei.close();
+        Serial.println("Datei erfolgreich gelesen und geschlossen.");
     }
     else
     {
-        Serial.println("Fehler beim schreiben!");
+        Serial.println("Fehler: Datei 'testMap' wurde nicht auf der SD-Karte gefunden!");
     }
 }
+
 void loop()
 {
 }
